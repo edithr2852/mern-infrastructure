a68ffb7409579dd34b4ad5fd2a2b98152a37a5a2
Dan Seagren <seagrendaniel562@gmail.com>
2021-04-23 14:31:10 -0700

    add jwt auth

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,29 @@
-function create(req, res) {
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  })
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+
+/*--- Helper Functions ---*/
+function createJWT(user) {
+  return jwt.sign(
+    // Data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
+}
+
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+    const token = createJWT(user)
+    res.json(token)
+  } catch (err) {
+    // Client will check for non-2xx status code
+    // 400 code  = bad request
+    res.status(400).json(err);
+  }
 }
 
 module.exports = {
-  create
-}
+  create,
+};
--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
-const UserSchema = new Schema(
+const SALT_ROUNDS = 6 // 6 is a typical and reasonable value
+
+const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: {
@@ -27,4 +30,17 @@ const UserSchema = new Schema(
   }
 );
 
-module.export = mongoose.model("User", UserSchema);
+userSchema.pre("save", function (next) {
+  // Save the reference to the user doc
+  const user = this;
+  if (!user.isModified("password")) return next();
+  // if password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+    if (err) return next(err);
+    // Update the password property with the hash
+    user.password = hash;
+    return next();
+  });
+});
+
+module.exports = mongoose.model("User", userSchema);
